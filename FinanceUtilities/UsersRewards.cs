888a4ffa302b39b2628733c7aba9e634da775bbc
Endur4nce <mizumelon1807@gmail.com>
2022-08-15 23:08:35 +0400

    Added UsersRewards

---
+++ FinanceUtilities/UsersRewards.cs
@@ -0,0 +1,19 @@
+﻿using Core;
+using Interfaces;
+
+namespace FinanceUtilities
+{
+    public class UsersRewards : IUsersRewards
+    {
+        public IEnumerable<User> AddRewards(IEnumerable<User> users, int percent)
+        {
+            foreach(var user in users)
+            {
+                var amount = int.Parse(user.GetAmount());
+                var totalSum = amount * ((double)percent / 100 + 1);
+                user.SetAmount(totalSum.ToString());
+                yield return user;
+            }
+        }
+    }
+}
