a8850c950d7a3ea5c5671ed5e36ae95f6ba6330a
Endur4nce <mizumelon1807@gmail.com>
2022-08-15 23:09:21 +0400

    Added Validator

---
+++ Validator/Validator.cs
@@ -0,0 +1,15 @@
+﻿using Interfaces;
+
+namespace InputProcessing
+{
+    public class Validator : IValidator
+    {
+        public bool IsValid(string line)
+        {
+            var data = line.Split('\t');
+            if (data.Length != 2) 
+                return false;
+            return int.TryParse(data[1], out _);
+        }
+    }
+}
