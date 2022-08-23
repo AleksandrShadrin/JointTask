39cf04150d52542d0bccd197c55180f913234be0
Михаил Котов <misha.kotov.01@list.ru>
2022-08-23 12:08:47 +0400

    add params loader

--- JointTask/Program.cs
+++ JointTask/Program.cs
@@ -5,7 +5,10 @@ using IOUtilities;
 using Interfaces;
 using IOUtilities.Exceptions;
 using Log;
+using Loader;
 
+
+//убрать логгер из валидатора
 namespace JointTask
 {
     
@@ -13,25 +16,34 @@ namespace JointTask
     {   
         public static void Main(string[] args)
         {
-
+            Console.ReadLine();
+            //Logs
             Action<string> ExceptionLogger;
             Logger logger = new ConsoleLogger();
             ExceptionLogger = logger.Log;
 
+            //LoadConfigure
+            ConfigLoader configLoader = new ConfigLoader();
+            configLoader.LoadConfig();
+
+            
 
             //читаем с файла
             IReader reader = new Reader(new Parser(), new Validator(ExceptionLogger));
-            IEnumerable<User> users = reader.Read(@"C:\Users\User\Documents\mveuC#\1660602157406.txt");
-            
+            //IEnumerable<User> users = reader.Read(@"C:\Users\User\Documents\mveuC#\1660602157406.txt");
+            IEnumerable<User> users = reader.Read(configLoader.SourceFile);
+
             //обрабатываем
             IUsersRewards usersRewards = new UsersRewards();
-            users = usersRewards.AddRewards(users, 20);
+            //users = usersRewards.AddRewards(users, 20);
+            users = usersRewards.AddRewards(users, configLoader.Reward);
 
             try
             {
                 long fileName = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
                 //запись в файл
-                IWriter writer = new Writer($@"C:\Users\User\Documents\mveuC#\{fileName}.txt");
+                //IWriter writer = new Writer($@"C:\Users\User\Documents\mveuC#\{fileName}.txt");
+                IWriter writer = new Writer(configLoader.ReportFile);
                 writer.Write(users);
                 //можно добавить из какого файла взяли
                 Console.WriteLine($"Обработка произошла успешно, данные выведены в файл {fileName}.txt");
---
+++ Loader/ConfigLoader.cs
@@ -0,0 +1,101 @@
+﻿namespace Loader
+{
+    public class ConfigLoader
+    {
+        public string SourceFile { get; private set; }
+        public string ReportFile { get; private set; }
+        private int reward;
+        private string[] inputData;
+
+        
+        public int Reward
+        {
+            get
+            {
+                return reward;
+            }
+            set
+            {
+                if (value >= 0)
+                    reward = value;
+                else
+                    throw new ArgumentException();
+            }
+        }
+
+
+        public ConfigLoader()
+        {
+            this.SourceFile = "";
+            this.ReportFile = $@"C:\Users\User\Documents\mveuC#\" + 
+                $@"{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString()}.txt";
+                
+            this.reward = 0;
+        }
+
+        public void LoadConfig()
+        {
+            SearchingSystemDirectory();
+            SearchingHomeDirectory();
+            GetFromParams();   
+        }
+
+
+        private void SearchingSystemDirectory()
+        {
+            string[] allFoundFiles = Directory.GetFiles(@"C:\ProgramData", "uniqueConfig.txt");
+            try
+            {
+                inputData = File.ReadAllLines(allFoundFiles[0] + @"\uniqueConfig.txt");
+                AssignValues(inputData);
+            }
+            catch (Exception)
+            {
+                //throw;
+            }
+        }
+        private void SearchingHomeDirectory()
+        {
+            string[] allFoundFiles = Directory.GetFiles(@"C:\Users\" + Environment.UserName + @"\AppData\Local", "uniqueConfig.txt");
+            try
+            {
+                inputData = File.ReadAllLines(allFoundFiles[0] + @"\uniqueConfig.txt");
+                AssignValues(inputData);
+            }
+            catch (Exception)
+            {
+                //throw;
+            }
+        }
+
+        private void GetFromParams()
+        {
+            this.AssignValues(string.Join(" ", Environment.GetCommandLineArgs()).Split("-"));
+        }
+
+        private void AssignValues(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                string[] values = arg.Split(" ");
+
+                if (!string.IsNullOrEmpty(values[1]))
+                    switch (values[0])
+                    {
+                        case "sourceFile":
+                            SourceFile = values[1];
+                            break;
+                        case "reportFile":
+                            ReportFile = values[1];
+                            break;
+                        case "reward":
+                            reward = int.Parse(values[1]);
+                            break;
+                    }
+            }
+        }
+
+
+
+    }
+}
