c6cfb19f843fb29962058dce5179c16fcce40ccf
Alex <ssaasshha11@yandex.ru>
2022-08-26 21:40:16 +0400

    Add interfaces and refactor code.

--- Loader/ConfigLoader.cs
+++ Loader/ConfigLoader.cs
@@ -1,101 +1,112 @@
-﻿namespace Loader
+﻿using System.Collections;
+
+namespace Loader
 {
-    public class ConfigLoader
+    public class ConfigLoader : ICloneable, IComparable<ConfigLoader>, IEnumerator<KeyValuePair<string, string>>
     {
-        public string SourceFile { get; private set; }
-        public string ReportFile { get; private set; }
-        private int reward;
-        private string[] inputData;
+        public KeyValuePair<string, string> Current => _config.ElementAtOrDefault(currentPosition);
 
-        
-        public int Reward
-        {
-            get
-            {
-                return reward;
-            }
-            set
-            {
-                if (value >= 0)
-                    reward = value;
-                else
-                    throw new ArgumentException();
-            }
-        }
-
-
-        public ConfigLoader()
-        {
-            this.SourceFile = "";
-            this.ReportFile = $@"C:\Users\User\Documents\mveuC#\" + 
-                $@"{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString()}.txt";
-                
-            this.reward = 0;
-        }
+        private Dictionary<string, string> _config = new();
+        private int currentPosition = 0;
+        object IEnumerator.Current => _config.ElementAtOrDefault(currentPosition);
 
         public void LoadConfig()
         {
-            SearchingSystemDirectory();
-            SearchingHomeDirectory();
-            GetFromParams();   
+            SearchInDirectory(Environment.SpecialFolder.ApplicationData);
+            SearchInDirectory(Environment.SpecialFolder.LocalApplicationData);
+            GetFromParams();
         }
 
-
-        private void SearchingSystemDirectory()
+        private ConfigLoader(Dictionary<string, string> config)
         {
-            string[] allFoundFiles = Directory.GetFiles(@"C:\ProgramData", "uniqueConfig.txt");
+            _config = config;
+        }
+
+        private void SearchInDirectory(Environment.SpecialFolder specialFolder)
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(specialFolder), "uniqueConfig.txt");
             try
             {
-                inputData = File.ReadAllLines(allFoundFiles[0] + @"\uniqueConfig.txt");
-                AssignValues(inputData);
+                StreamReader sr;
+                using (sr = File.OpenText(filePath))
+                {
+                    string str;
+                    while (String.IsNullOrWhiteSpace(str = sr.ReadLine()) == false)
+                    {
+                        TryAssignValue(str);
+                    }
+                }
             }
             catch (Exception)
             {
                 //throw;
             }
         }
-        private void SearchingHomeDirectory()
-        {
-            string[] allFoundFiles = Directory.GetFiles(@"C:\Users\" + Environment.UserName + @"\AppData\Local", "uniqueConfig.txt");
-            try
-            {
-                inputData = File.ReadAllLines(allFoundFiles[0] + @"\uniqueConfig.txt");
-                AssignValues(inputData);
-            }
-            catch (Exception)
-            {
-                //throw;
-            }
-        }
-
         private void GetFromParams()
         {
-            this.AssignValues(string.Join(" ", Environment.GetCommandLineArgs()).Split("-"));
-        }
-
-        private void AssignValues(string[] args)
-        {
-            foreach (string arg in args)
+            var values = string.Join(" ", Environment.GetCommandLineArgs()).Split("-");
+            if (values[0] != null)
             {
-                string[] values = arg.Split(" ");
-
-                if (!string.IsNullOrEmpty(values[1]))
-                    switch (values[0])
-                    {
-                        case "sourceFile":
-                            SourceFile = values[1];
-                            break;
-                        case "reportFile":
-                            ReportFile = values[1];
-                            break;
-                        case "reward":
-                            reward = int.Parse(values[1]);
-                            break;
-                    }
+                TryAssignValue("sourceFile" + values[0]);
+            }
+            if (values[1] != null)
+            {
+                TryAssignValue("reportFile" + values[1]);
+            }
+            if (values[2] != null)
+            {
+                TryAssignValue("reward" + values[2]);
             }
         }
 
+        private void TryAssignValue(string value)
+        {
+            string[] values = value.Split(" ");
+            if (!string.IsNullOrEmpty(values[1]))
+                switch (values[0])
+                {
+                    case "sourceFile":
+                        _config.Add("sourceFile", values[1]);
+                        break;
+                    case "reportFile":
+                        _config.Add("reportFile", values[1]);
+                        break;
+                    case "reward":
+                        _config.Add("reward", values[1]);
+                        break;
+                }
+        }
 
 
+        public int CompareTo(ConfigLoader? other)
+        {
+            if (other is not null)
+            {
+                return this._config.Count - other._config.Count;
+            }
+            return 1;
+        }
+
+        public object Clone()
+        {
+            return new ConfigLoader(this._config);
+        }
+
+        public bool MoveNext()
+        {
+            if (_config.Count >= currentPosition)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Reset()
+        {
+            currentPosition = 0;
+        }
+        public void Dispose()
+        {
+            _config.Clear();
+        }
     }
 }
